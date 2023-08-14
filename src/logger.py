380e1b099e81690d7085298e8789c12e60dd1f99
Eduardo Gomes <edu_merckx@yahoo.com.br>
2023-08-14 00:08:48 -0300

    chore: update format logger

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_logger():
-    FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+    FORMAT = '%(asctime)s %(message)s'
     logging.basicConfig(format=FORMAT)
     logger = logging.getLogger(__name__)
     LOG_LEVEL = os.getenv('LOG_LEVEL')
