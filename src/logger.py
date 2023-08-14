6c2597db4390d57da1fb3fec705b29b1ff5c251b
Eduardo Gomes <edu_merckx@yahoo.com.br>
2023-08-14 12:40:25 -0300

    chore: update logger

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,9 @@ def get_logger():
     FORMAT = '%(asctime)s %(message)s'
     logging.basicConfig(format=FORMAT)
     logger = logging.getLogger(__name__)
-    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    LOG_LEVEL = (
+        os.getenv('LOG_LEVEL') if 'LOG_LEVEL' in os.environ else 'DEBUG'
+    )
     logger.setLevel(LOG_LEVEL)
 
     return logger
